52fa18c9760fe649c9ad7d68bf6f16b1adafe3cd
Roman <roman.ermeev.dev@gmail.com>
2019-11-17 14:16:09 -0500

    folder structure was created, webpack config file was created

---
+++ backend/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\category;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function(){
+    return view('main_page.main_page');
+});
---
+++ frontend/src/index.js
@@ -0,0 +1,3 @@
+import Image from "./assets/2.jpg";
+
+document.querySelector('#newImage').src = 'assets/2.jpg';
